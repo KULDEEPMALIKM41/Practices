6351347035ac9299fcb79e3ef5004042acf76141
Kuldeep Mali <Kuldeepmalikm41@gmail.com>
2020-08-24 21:15:13 +0530

    Solve Redis Server Issue in django-channels-example

--- Python/django-channels-example/chat/routing.py
+++ Python/django-channels-example/chat/routing.py
@@ -1,8 +1,18 @@
 # chat/routing.py
 from django.urls import re_path
-
 from . import consumers
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
 ]
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
--- Python/django-channels-example/mysite/settings.py
+++ Python/django-channels-example/mysite/settings.py
@@ -42,16 +42,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-# mysite/settings.py
-# Channels
-ASGI_APPLICATION = 'mysite.routing.application'
+
+ASGI_APPLICATION = 'chat.routing.application'
+
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
 
 MIDDLEWARE = [
